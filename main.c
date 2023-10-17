086456eb3ba2b807db24158494cf74fd95cad425
FatihSir <mohamedalfateh83@gmail.com>
2023-10-17 16:42:05 -0700

    Task 0 dixes

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include <limits.h>
+/**
+* main - testing function
+* Return: always returns 0
+*
+*/
 int main(void)
 {
-	_printf("This %d %s %d but %% %c\n",193,"and",296,'A');
+	_printf("This %d %s %d but %% %c\n", 193, "and", 296, 'A');
 	return (0);
 }
