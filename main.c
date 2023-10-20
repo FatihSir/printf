89a6474f2522c1efa698009262539da345d87e87
FatihSir <mohamedalfateh83@gmail.com>
2023-10-20 13:16:26 -0700

    Task 0

--- main.c
+++ main.c
@@ -1,24 +1,42 @@
+#include <limits.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
  * main - Entry point
  *
- * Return: 0 on success, error code otherwise
+ * Return: Always 0
  */
 int main(void)
 {
-	int len, len2;
+	int len;
+	int len2;
+	char C = 'C';
+	char *str = "Let's try to printf a simple sentence.\n";
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len2);
+	printf("Length:[%d, %i]\n", len, len2);
 
-	len = _printf("%!\n");
-	len2 = printf("%!\n");
-	fflush(stdout);
-	if (len != len2)
-	{
-		printf("Lengths differ.\n");
-		fflush(stdout);
-		return (1);
-	}
+	len = _printf("Character:[%c]\n", 'H');
+	len2 = printf("Character:[%c]\n", 'H');
+	_printf("Length:[%d, %i]\n", len, len2);
+	printf("Length:[%d, %i]\n", len, len2);
+
+	len = _printf("String:[%s][%c][%d][%s]\n", "I am a string !",C,-300,str);
+	len2 = printf("String:[%s][%c][%d][%s]\n", "I am a string !",C,-300,str);
+	_printf("Length:[%d, %i]\n", len, len2);
+	printf("Length:[%d, %i]\n", len, len2);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Length:[%d, %i]\n", len, len2);
+	printf("Length:[%d, %i]\n", len, len2);
+	len = _printf("Negative:[%d]\n", -762534);
+	len2 = printf("Negative:[%d]\n", -762534);
+	_printf("Length:[%d, %i]\n", len, len2);
+	printf("Length:[%d, %i]\n", len, len2);
+
+
 	return (0);
+ 
 }
